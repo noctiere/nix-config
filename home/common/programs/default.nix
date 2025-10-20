089dc61088c7069b25f5b710e2cede91d5581b49
noctiere <potatoyear@gmail.com>
2025-10-20 15:56:37 +0300

    add zed-editor common hm module

--- home/common/programs/default.nix
+++ home/common/programs/default.nix
@@ -1,6 +1,7 @@
 {
   imports = [
     ./codium.nix
+    ./zed-editor.nix
     ./git.nix
     ./helix.nix
     ./spicetify.nix
--- home/noctiere/config.nix
+++ home/noctiere/config.nix
@@ -7,6 +7,12 @@
     };
     programs = {
       vscodium.enable = true;
+      zed-editor = {
+        enable = true;
+        extensions = ["toml" "codebook"];
+        fontSize = 16;
+        fontFamily = "JetBrainsMono Nerd Font";
+      };
     };
   };
 }

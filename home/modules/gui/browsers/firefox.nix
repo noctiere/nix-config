3ebea7ebb9dd4434395a14b749c3c7a9db2153d3
noctiere <potatoyear@gmail.com>
2026-02-19 09:59:09 +0300

    firefox update policies and extensions by default

--- home/modules/gui/browsers/firefox.nix
+++ home/modules/gui/browsers/firefox.nix
@@ -5,6 +5,19 @@
   ...
 }: let
   cfg = config.hmModules.gui.browsers.firefox;
+
+  lock-false = {
+    Value = false;
+    Status = "locked";
+  };
+  lock-true = {
+    Value = true;
+    Status = "locked";
+  };
+  lock-empty-string = {
+    Value = "";
+    Status = "locked";
+  };
 in {
   options.hmModules.gui.browsers.firefox = {
     enable = lib.mkEnableOption "Whether to enable Firefox";
@@ -15,6 +28,42 @@ in {
 
     programs.firefox = {
       enable = true;
+      policies = {
+        DisableTelemetry = true;
+        DisableFirefoxStudies = true;
+        DontCheckDefaultBrowser = true;
+        DisablePocket = true;
+        SearchBar = "unified";
+
+        Preferences = {
+          # Privacy settings
+          "extensions.pocket.enabled" = lock-false;
+          "browser.newtabpage.pinned" = lock-empty-string;
+          "browser.topsites.contile.enabled" = lock-false;
+          "browser.newtabpage.activity-stream.showSponsored" = lock-false;
+          "browser.newtabpage.activity-stream.system.showSponsored" = lock-false;
+          "browser.newtabpage.activity-stream.showSponsoredTopSites" = lock-false;
+        };
+
+        ExtensionSettings = {
+          "uBlock0@raymondhill.net" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+            installation_mode = "force_installed";
+          };
+          "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+            installation_mode = "force_installed";
+          };
+          "jid1-MnnxcxisBPnSXQ@jetpack" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
+            installation_mode = "force_installed";
+          };
+          "extension@tabliss.io" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/file/3940751/tabliss-2.6.0.xpi";
+            installation_mode = "force_installed";
+          };
+        };
+      };
     };
   };
 }

15b83768234cc6a2b1e8a9941d2c8b49ebfff1ef
noctiere <potatoyear@gmail.com>
2026-02-23 11:05:39 +0300

    move xdg settiong into hyprland

--- modules/nixos/gui/wm/hyprland.nix
+++ modules/nixos/gui/wm/hyprland.nix
@@ -42,11 +42,38 @@ in {
       trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
     };
 
+    xdg = {
+      autostart.enable = lib.mkDefault true;
+      menus.enable = lib.mkDefault true;
+      mime.enable = lib.mkDefault true;
+      icons.enable = lib.mkDefault true;
+    };
+
     xdg.portal = {
       enable = true;
+
+      config = {
+        common = {
+          # Use xdg-desktop-portal-gtk for every portal interface...
+          default = [
+            "gtk"
+            # "gnome"
+          ];
+        };
+      };
+
+      # Sets environment variable NIXOS_XDG_OPEN_USE_PORTAL to 1
+      # This will make xdg-open use the portal to open programs,
+      # which resolves bugs involving programs opening inside FHS envs or with unexpected env vars set from wrappers.
+      # xdg-open is used by almost all programs to open a unknown file/uri
+      # alacritty as an example, it use xdg-open as default, but you can also custom this behavior
+      # and vscode has open like `External Uri Openers`
       xdgOpenUsePortal = true;
-      extraPortals = [
-        # pkgs.xdg-desktop-portal-gtk
+
+      # ls /run/current-system/sw/share/xdg-desktop-portal/portals/
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-gtk # for provides file picker / OpenURI
+        # xdg-desktop-portal-gnome # for screensharing
       ];
     };
 
